bef990e3a5ed8cb57c9432eacc021ef0b770f7b6
nikthewik <nikthewik@gmail.com>
2024-09-19 13:38:43 +0200

    Modify state management in Projects section.

--- src/components/ProjectsFilter/ProjectsFilter.tsx
+++ src/components/ProjectsFilter/ProjectsFilter.tsx
@@ -1,15 +1,33 @@
+// Libraries
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+// Types
+import { AppDispatch, RootState } from "../../store";
+// Slices
+import { toggleTag } from "../../features/tags/tagsSlice";
 // Data
 import tags from "../../data/arrays/tags";
 // Styles
 import styles from "./ProjectsFilter.module.css";
-// Components
-import TagBtn from "../TagBtn/TagBtn";
 
-function ProjectsFilter() {
+function ProjectsFilter(): React.ReactElement {
+  const { tags: tagsState } = useSelector((state: RootState) => state.tags);
+  const dispatch: AppDispatch = useDispatch();
+
   return (
     <div className={styles.filterContainer}>
       {tags.map((tag) => {
-        return <TagBtn key={tag}>{tag}</TagBtn>;
+        return (
+          <button
+            className={`${styles.btn} ${
+              tagsState.includes(tag) ? styles.btnPressed : styles.btnNotPressed
+            }`}
+            onClick={() => dispatch(toggleTag(tag))}
+            key={tag}
+          >
+            {tag}
+          </button>
+        );
       })}
     </div>
   );
